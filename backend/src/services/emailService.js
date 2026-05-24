3e0d392ea50f9a33d204bbfcc21d49f1457bd34d
Niviru I.U. Weerasinghe <niviruedu2006@gmail.com>
2026-05-25 00:18:43 +0530

    Force IPv4 for Nodemailer DNS resolution to fix Render ENETUNREACH

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -1,5 +1,9 @@
 const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
+const dns = require('dns');
+
+dns.setDefaultResultOrder('ipv4first'); // Force IPv4 to prevent Render IPv6 ENETUNREACH issues
+
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
